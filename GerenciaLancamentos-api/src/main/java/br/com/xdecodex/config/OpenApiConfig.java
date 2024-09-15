1e047f3772fd8466a7b5c2ac57f6185e2b969eac
TiagoTSR <craimos3001@gmail.com>
2024-09-15 13:26:30 -0300

    Configuração Básica Swagger(Open API)

---
+++ GerenciaLancamentos-api/src/main/java/br/com/xdecodex/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package br.com.xdecodex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+	
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("RESTful API with Java 21 and Spring Boot 4")
+				.version("v1")
+				.description("Some description about your API")
+				.termsOfService("https://pub.xdecodex.com.br/meus-cursos")
+				.license(
+					new License()
+						.name("Apache 2.0")
+						.url("https://pub.xdecodex.com.br/meus-cursos")
+					)
+				);
+	}
+}
